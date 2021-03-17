9408275bea2290942aaa47644c57310f1299d352
pboennig <pboennig@gmail.com>
2021-03-16 16:15:57 -1000

    decompose getPhoto

--- App/Styles/keyStyles.js
+++ App/Styles/keyStyles.js
@@ -1,6 +1,35 @@
-import {Dimensions} from 'react-native';
+import React from 'react';
+import {
+    Image,
+    Dimensions
+} from 'react-native';
+
 // TODO: import typography for lato?
 import { useFonts, Lato_400Regular, Lato_400Bold } from '@expo-google-fonts/lato';
+
+let PIC_SIZE = 20;
+
+export function getPhoto(username) {
+    var photoDict = new Map();
+    let johnPhoto = <Image source={require("../Images/John.png")} style={keyStyles.pictureStyle}/>
+    photoDict.set("rachel_f", <Image source={require("../Images/Rachel.png")} style={keyStyles.pictureStyle}/>)
+    photoDict.set("paul_walrus", <Image source={require("../Images/Paul.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("george_h", <Image source={require("../Images/George.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("jerry_g", <Image source={require("../Images/Jerry.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("phil_l", <Image source={require("../Images/Phil.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("weir_wood", <Image source={require("../Images/Bob.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("p_sing", <Image source={require("../Images/Singer.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("jacques_d", <Image source={require("../Images/Derrida.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("bentham", <Image source={require("../Images/Bentham.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("gordon_r", <Image source={require("../Images/Gordon.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("gusteau", <Image source={require("../Images/Gusteau.png")} style={keyStyles.pictureStyle}/>);
+    photoDict.set("marco", <Image source={require("../Images/Marco.png")} style={keyStyles.pictureStyle}/>);
+    if (photoDict.has(username)) {
+        return photoDict.get(username);
+    } else {
+        return johnPhoto;
+    }
+}
 
 const keyStyles = {
     DEFAULT_BORDER_RADIUS: 20,
@@ -11,6 +40,7 @@ const keyStyles = {
     LIGHT_GRAY: '#F2F2F2',
     SALMON_COLOR:"#f59376", //  #f59376 // #F98562
     DARK_GRAY: '#979797',
+    PIC_SIZE: 20,
     // const ICON_SIZE: 24,
 
     topRight: {
@@ -79,6 +109,11 @@ const keyStyles = {
         width: '100%',
         height: '100%'
     },
+    pictureStyle: {
+        resizeMode: 'contain',
+        height: PIC_SIZE,
+        width: PIC_SIZE
+    }
 }
 
 export default keyStyles
