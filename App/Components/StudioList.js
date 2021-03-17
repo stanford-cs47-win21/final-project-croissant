2eeb9c841b4e6af2e9de56681da3efb6dff5e1b3
Sejal Jhawer <sejaljhawer@gmail.com>
2021-03-17 02:15:01 -0700

    Init fan invite

--- App/Components/StudioList.js
+++ App/Components/StudioList.js
@@ -11,13 +11,16 @@ import { StyleSheet,
 import {StudioCard} from "./StudioCard";
 import {PlusButton} from "./PlusButton";
 import {UpcomingStudio} from "./UpcomingStudio"
-
+import {FanInvite} from "./FanInvite";
 
 export function StudioList({fan=false, ...props}) {
     const studios = props.studios;
 
     const renderStudioCard = ({index, item}) => {
         // remove the plus button altogether if fan
+        if (item.isInvite) {
+            return <FanInvite inviteInfo={item}/>
+        } else 
         if (item.isAlert) {
             return <UpcomingStudio alertInfo={{numParticipants:item.status, time:item.message, date:item.timeLeft}}/>
         } else if (fan && index === studios.length - 1) {
